0690a27f246ed40cb2c15827ca5804c7da852ab9
yxchandev <gnomemage1990@gmail.com>
2025-03-10 10:26:07 +0800

    update font size

--- src/app/(frontend)/[locale]/aboutus/page.tsx
+++ src/app/(frontend)/[locale]/aboutus/page.tsx
@@ -23,7 +23,7 @@ export default async function AboutPage({ params }: { params: { locale: string }
               <BreadcrumbSeparator />
               <BreadcrumbPage>{aboutUs?.title}</BreadcrumbPage>
             </Breadcrumb>
-            <div className="text-3xl font-semibold my-2 text-txt-black-900">
+            <div className="text-heading-md font-semibold my-2 text-txt-black-900">
               <h1>{aboutUs?.title}</h1>
             </div>
           </div>
@@ -36,8 +36,8 @@ export default async function AboutPage({ params }: { params: { locale: string }
           </figure>
 
           <div className="flex flex-col gap-[24px] my-6">
-            <h2 className="font-semibold text-3xl text-txt-black-900">{aboutUs?.["intro-header"]}</h2>
-            <div className="font-normal text-base leading-[26px] text-txt-black-700">
+            <h2 className="font-semibold text-heading-md text-txt-black-900">{aboutUs?.["intro-header"]}</h2>
+            <div className="font-normal text-heading-2xs leading-[26px] text-txt-black-700">
               {aboutUs?.["intro-description"]}
             </div>
           </div>
@@ -49,7 +49,7 @@ export default async function AboutPage({ params }: { params: { locale: string }
                 <div className="flex items-center justify-start w-auto h-20 bg-orange-100 rounded-full mb-4">
                   <img src={value?.["image-link"]} className="icon" alt={value?.["card-header"]} />
                 </div>
-                <h3 className="my-4 text-xl font-semibold text-txt-black-900">{value?.["card-header"]}</h3>
+                <h3 className="my-4 text-heading-xs font-semibold text-txt-black-900">{value?.["card-header"]}</h3>
                 <p className="text-txt-black-700 text-base font-normal leading-normal">
                   {value?.["card-description"]}
                 </p>
@@ -58,7 +58,7 @@ export default async function AboutPage({ params }: { params: { locale: string }
           </div>
 
           {/* Our Values */}
-          <h3 className="mb-2 text-xl font-semibold text-txt-black-900">{aboutUs?.our_values}</h3>
+          <h3 className="mb-2 text-heading-md font-semibold text-txt-black-900">{t('our_values')}</h3>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 sm:grid-cols-2 mb-12">
             {aboutUs["our-values"].map((value: {[key: string]: string}, index: number) => (
               <div key={index} className="p-6 border border-gray-200 rounded-lg shadow-sm flex flex-row gap-4">
@@ -66,7 +66,7 @@ export default async function AboutPage({ params }: { params: { locale: string }
                   <img src={value?.["image-link"]} className="icon" alt={value?.["card-header"]} />
                 </div>
                 <div className="flex-1">
-                  <h3 className="mb-2 text-xl font-semibold text-txt-black-900">{value?.["card-header"]}</h3>
+                  <h3 className="mb-2 text-heading-xs font-semibold text-txt-black-900">{value?.["card-header"]}</h3>
                   <p className="text-txt-black-700">
                     {value?.["card-description"]}
                   </p>
@@ -77,7 +77,7 @@ export default async function AboutPage({ params }: { params: { locale: string }
 
           {/* Our Community */}
           <section className="max-w-6xl mx-auto px-4 pt-8 text-left">
-            <h2 className="text-3xl font-semibold mb-2 text-txt-black-900">{t('our_community')}</h2>
+            <h2 className="text-heading-md font-semibold mb-2 text-txt-black-900">{t('our_community')}</h2>
           </section>
 
           <section className="max-w-6xl mx-auto px-4 py-12">
@@ -108,7 +108,7 @@ const KadKommuniti=async({community_text}: { community_text: string })=>{
             className="rounded-md border-2 border-orange-500"
         />
         <div
-            className="mt-4 inline-flex items-center text-txt-black-900 font-medium"
+            className="mt-4 inline-flex items-center text-txt-black-900 font-medium text-heading-xs"
         >
           {community_text}
           <span className="ml-1">→</span>
