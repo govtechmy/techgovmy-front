cf3de9f39f5eb91c97b25ddc41168135b7bd486b
Amaluddin Asyraf <amaluddinasyraf@gmail.com>
2025-04-17 20:36:17 +0800

    fix padding in contact page

--- src/app/(frontend)/[locale]/contact-us/page.tsx
+++ src/app/(frontend)/[locale]/contact-us/page.tsx
@@ -16,19 +16,19 @@ export default async function ContactPage({ params }: ServerPageProps) {
 
   return (
     <div className="w-full bg-gradient-to-b from-white to-[#EFF6FF]">
-      <div className="w-full border-otl-divider max-w-screen-xl px-10 md:px-[5.25rem] lg:px-[6.8125rem]  mx-auto justify-center space-y-8 md:space-y-16 pt-8 md:pt-10 lg:pt-16 pb-9">
-        <section className="gap-16 lg:gap-0 flex-col lg:flex-row mx-auto flex bg-white rounded-xl border border-otl-divider justify-between lg:w-[70.375rem] lg:h-[32.75rem]">
-          <div className="lg:w-[18.875rem] lg:h-[23rem] flex flex-col md:flex-row lg:flex-col w-full gap-12 lg:gap-0 items-start justify-start">
-            <div className="pt-8 px-8">
-              <h1 className="text-heading-md font-semibold text-txt-black-900 pb-8">
+      <div className="mx-auto w-full max-w-screen-xl justify-center space-y-8 border-otl-divider px-10 pt-8 md:space-y-16 md:px-[5.25rem] md:pt-16 lg:px-[6.8125rem] lg:py-[5.25rem]">
+        <section className="mx-auto flex flex-col justify-between gap-16 rounded-xl border border-otl-divider bg-white lg:h-[32.75rem] lg:w-[70.375rem] lg:flex-row lg:gap-0">
+          <div className="flex w-full flex-col items-start justify-start gap-12 md:flex-row lg:h-[23rem] lg:w-[18.875rem] lg:flex-col lg:gap-0">
+            <div className="px-8 pt-8">
+              <h1 className="pb-8 text-heading-md font-semibold text-txt-black-900">
                 {t("contact_us")}
               </h1>
-              <div className="flex flex-col md:flex-row lg:flex-col sm:gap-12 lg:gap-0">
-                <div>
-                  <h1 className="text-txt-black-900 text-body-xl font-semibold">
+              <div className="flex flex-col gap-12 md:flex-row lg:flex-col lg:gap-0">
+                <div className="flex-1">
+                  <h1 className="text-body-xl font-semibold text-txt-black-900">
                     {t("office_name")}
                   </h1>
-                  <p className="text-txt-black-700 text-body-md font-light pt-2">
+                  <p className="pt-2 text-body-md font-light text-txt-black-700">
                     {contact.address &&
                       contact.address.split(",").map((line: string, index: number) => (
                         <React.Fragment key={index}>
@@ -37,38 +37,38 @@ export default async function ContactPage({ params }: ServerPageProps) {
                         </React.Fragment>
                       ))}
                   </p>
-                  <div className="flex gap-4 text-primary-500 text-body-md font-normal mt-4.5 pt-3">
+                  <div className="mt-4.5 flex gap-4 pt-3 text-body-md font-normal text-primary-500">
                     <a
                       href={contact.google_maps_url!}
                       target="_blank"
-                      className="flex gap-2 w-auto items-center"
+                      className="flex w-auto items-center gap-2"
                     >
-                      <DirectionIcon className="size-8 text-primary-500 rounded-full bg-primary-50 p-1.5" />
+                      <DirectionIcon className="size-8 rounded-full bg-primary-50 p-1.5 text-primary-500" />
                       Google Maps
                     </a>
                     <a
                       href={contact.waze_url!}
                       target="_blank"
-                      className="flex gap-2 w-auto items-center"
+                      className="flex w-auto items-center gap-2"
                     >
-                      <DirectionIcon className="size-8 text-primary-500 rounded-full bg-primary-50 p-1.5" />
+                      <DirectionIcon className="size-8 rounded-full bg-primary-50 p-1.5 text-primary-500" />
                       Waze
                     </a>
                   </div>
                 </div>
-                <div className="w-[18.875rem] h-[7.75rem] lg:pt-12 md:pl-8 lg:pl-0">
-                  <h1 className="text-txt-black-900 text-body-xl font-semibold">
+                <div className="h-[7.75rem] w-[18.875rem] flex-1 lg:pt-12">
+                  <h1 className="text-body-xl font-semibold text-txt-black-900">
                     {t("inquiries_feedback")}
                   </h1>
                   <div className="flex flex-col gap-4 pt-4 text-body-md">
                     <a
                       href={`mailto:${contact.email}`}
-                      className="flex gap-2 w-auto items-center text-primary-500"
+                      className="flex w-auto items-center gap-2 text-primary-500"
                     >
                       <EmailIcon className="size-8 rounded-full bg-primary-50 p-1.5" />
                       {contact.email}
                     </a>
-                    <div className="flex gap-2 w-auto items-center text-primary-500">
+                    <div className="flex w-auto items-center gap-2 text-primary-500">
                       <PhoneIcon className="size-8 rounded-full bg-primary-50 p-1.5" />
                       {contact.phone}
                     </div>
@@ -78,7 +78,7 @@ export default async function ContactPage({ params }: ServerPageProps) {
             </div>
           </div>
           <iframe
-            className="w-full lg:w-[39.25rem] h-[21.875rem] lg:h-[32.75rem] md:h-[25rem] rounded-xl border border-gray-200"
+            className="h-[21.875rem] w-full rounded-xl border border-gray-200 md:h-[25rem] lg:h-[32.75rem] lg:w-[39.25rem]"
             loading="lazy"
             allowFullScreen
             referrerPolicy="no-referrer-when-downgrade"
