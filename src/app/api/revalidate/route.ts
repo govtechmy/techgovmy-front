272b8fad15902d911cb48011e70025c460a47bce
yxchandev <chanyuxiang6@gmail.com>
2025-04-28 13:18:30 +0800

    dynamically revalidate all individual product pages

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,11 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache'
+import { getPayload } from "payload";
+import config from "@payload-config";
 
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const { path } = body;
+
+    // get all product id because we need to revalidate all product pages
+    const payload = await getPayload({ config });
+    const products = await payload.find({
+      collection: 'products',
+      limit: 1000,
+    });
+
+    const productIds = products.docs.map(product => product.id);
 
     if (path === null || path === undefined) {
       return NextResponse.json({ message: 'Missing path' }, { status: 400 });
@@ -16,6 +27,9 @@ export async function POST(req: NextRequest) {
         revalidatePath("/about-us")
         revalidatePath("/contact-us")
         revalidatePath("/products")
+        for (const productId of productIds) {
+            revalidatePath(`/products/${productId}`);
+        }
     } else {
         revalidatePath(path)
     }
