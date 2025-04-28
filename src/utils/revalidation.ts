5990ddc62ce11ec168e4a14045237e2b8a6edce7
yxchandev <chanyuxiang6@gmail.com>
2025-04-28 16:03:16 +0800

    use server actions

--- src/utils/revalidation.ts
+++ src/utils/revalidation.ts
@@ -1,16 +1,18 @@
-export const revalidate = async (slug: string = "") => {
-    try {
-      await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/revalidate`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          path: `/${slug}`,
-        }),
-      });
-    } catch (error) {
-      console.error('Error revalidating from Global:', error);
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { getPayload } from "payload";
+import config from "@payload-config";
+
+export const revalidate = async (path: string = "", all: boolean = false) => {
+  try {
+    if (all) {
+        revalidatePath("/[locales]", "layout");
     }
-  };
-  
+    if (path) {
+        revalidatePath(path);
+    }
+  } catch (error) {
+    console.error("Error during revalidation:", error);
+  }
+};
