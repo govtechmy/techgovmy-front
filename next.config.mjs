009f17abdfe3ee2ec374368be738583efe42888a
yxchandev <chanyuxiang6@gmail.com>
2025-04-29 13:38:21 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  experimental: {
+    serverActions: true,
+  },
 };
 
 export default withPayload(withNextIntl(nextConfig));
