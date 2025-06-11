23e1d0feceb5f19006b1667a37fdcf6c386f9ecf
harrisazmi <azmi.harris@gmail.com>
2025-06-11 11:09:01 +0800

    GNU-1322 feat:metric-data

--- src/app/(frontend)/[locale]/products/[productId]/page.client.tsx
+++ src/app/(frontend)/[locale]/products/[productId]/page.client.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Tag } from "@govtechmy/myds-react/tag";
-import { ArrowBackIcon, ArrowOutgoingIcon } from "@govtechmy/myds-react/icon";
+import { ArrowBackIcon, ArrowOutgoingIcon, SwapIcon } from "@govtechmy/myds-react/icon";
 import ProductImages from "./product-images";
-import Timeline from "./timeline";
 import Image from "next/image";
 import { Product } from "@/payload-types";
 import { useTranslations } from "next-intl";
 import { DateTime } from "luxon";
+import { useState } from "react";
+import Metric from "@/components/products/metric";
 
 export default function ProductPageClient({ product }: { product: Product }) {
   const router = useRouter();
   const t = useTranslations("Product");
+  //later fix type in use state
+
+  interface sampleMetadataInterface {
+    year: number;
+    quarter: string;
+    data: any;
+    id: string;
+  }
 
   return (
     <div className="mx-auto grid w-full grid-cols-4 gap-4.5 md:grid-cols-8 md:gap-6 lg:grid-cols-12">
@@ -42,8 +51,7 @@ export default function ProductPageClient({ product }: { product: Product }) {
             <ArrowBackIcon className="" />
             <p>{t("back")}</p>
           </div>
-
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
             <div className="flex size-[72px] items-center justify-center rounded-md border border-otl-gray-200 bg-bg-dialog-active p-2 shadow-card">
               <Image
                 alt={`${product.name}-logo`}
@@ -53,7 +61,6 @@ export default function ProductPageClient({ product }: { product: Product }) {
                 className="absolute size-[56px] object-contain"
               />
             </div>
-
             <div className="space-y-3">
               <h1 className="text-heading-xs font-semibold">{product.name}</h1>
               <p className="text-sm text-txt-black-500">{product.description}</p>
@@ -67,7 +74,6 @@ export default function ProductPageClient({ product }: { product: Product }) {
                 <ArrowOutgoingIcon className="size-3 transition-transform group-hover:size-4" />
               </Tag>
             </div>
-
             <div className="block lg:hidden">
               <ProductImages
                 images={product.images.map(
@@ -76,24 +82,8 @@ export default function ProductPageClient({ product }: { product: Product }) {
               />
             </div>
 
-            <div className="hidden h-px bg-otl-divider lg:block" />
-
-            <div className="space-y-1.5">
-              <h6 className="text-body-sm font-semibold">{t("project-start")}</h6>
-              <p className="text-sm text-txt-black-500">
-                {DateTime.fromISO(product.project_start_date).toFormat("MMM yyyy")}
-              </p>
-            </div>
-
             <div className="h-px bg-otl-divider" />
-
-            <div className="space-y-1.5">
-              <h6 className="text-body-sm font-semibold">{t("project-status")}</h6>
-              <Timeline
-                items={product.status ? product.status?.map((stat) => stat.status) : []}
-                active={product.status ? product.status.findIndex((stat) => stat.isActive) : 0}
-              />
-            </div>
+            <Metric></Metric>
           </div>
         </div>
       </div>
